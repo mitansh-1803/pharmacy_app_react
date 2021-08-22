67d35aa9de9355827e6cff1cfaa8c4c803b9bd3c
mitansh-18 <mitanshrajpoot8@gmail.com>
2021-08-22 16:29:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Login from './components/login';
+import TopBar from './components/topbar';
+import Footer from './components/footer';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SaleExecutive from './components/salesExecutive';
+import StoreManager from './components/storeManager';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem("isLoggedIn")===null?false:localStorage.getItem("isLoggedIn"))
+
+  const updateLoggedIn = () =>{
+    setIsLoggedIn(localStorage.getItem("isLoggedIn"));
+  }
+
+  return (<>
+  <BrowserRouter>
+    <TopBar isLoggedIn={isLoggedIn} updateLoginStatus={updateLoggedIn}/>
+    <Switch>
+      <Route path="/" exact render={()=> <Login updateLoginStatus={updateLoggedIn} />} />
+      <div className="main-container">
+        <Route path="/salesexecutive" component={SaleExecutive} />
+        <Route path="/storemanager" component={StoreManager} />
+      </div>
+    </Switch>
+    <Footer />
+  </BrowserRouter>
+  </>);
 }
 
 export default App;
